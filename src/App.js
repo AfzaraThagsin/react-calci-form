c82637662ded5f8b1ef92389891994dd6991149d
AfzaraThagsin <afzarashajahan@gmail.com>
2023-04-10 15:10:31 +0530

    -Initial commit is done to save a file structure.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
+import HomeComponent from './Components/HomeComponent';
+import Calculator from './Components/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        
+        <ul>
+          <li>
+            <Link to='/Home' className='link'>Home</Link>
+          </li>
+          <li>
+            <Link to='/Calculator' className='link'>Calculator</Link>
+          </li>
+          <li>
+            <Link to='/cartoon' className='link'>cartoon</Link>
+          </li>
+         
+          
+        </ul>
+        <Routes>
+          <Route exact path='/Home' element={<HomeComponent/>}></Route>
+          <Route exact path='/Calculator' element={<Calculator/>}></Route>
+
+         
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/HomeComponent.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function HomeComponent() {
+  return (
+    <div><b>Welcome To My Profile</b>
+    <br/>
+    <br/>
+     <corner>Name : AFZARA THAGSIN.J.S<br/>
+    Register No : 212221040006<br/>
+    Department: Computer Science <br/></corner>
+    <br/>
+    <br/>
+    <b><u>CAREER OBJECTIVE</u></b><br/>
+      To Build a Career in a growing organization,Where I can gt the oppurtunity to prove my Innovative and Creative Abilities.<br/>
+
+      <h3><u>PERSONAL INFORMATION</u></h3><br/>
+     Father's Name : Shajahan.J<br/>
+Mother's Name : MehathaJunnisa.S<br/>
+Date Of Birth : 03.02.04<br/>
+Age : 18<br/>
+Permanent Address : 2/906f1,Kumaran 3rd Street,S.r.nagar[North],<br/>
+Mangalam Road,Andipalayam,Tiruppur-641 687<br/>
+Languages Known : English,Tamil,Urdu<br/>
+Gender : Female<br/>
+Martial Status : Single<br/>
+Nationality : Indian<br/>
+Contact No : 63742 54233<br/>
+
+<h3><u>ACADEMIC QUALIFICATION</u></h3><br/>
+<b>2021-2025 </b> :Pursuing B.E.Computer Science Engineering in Saveetha Engineering College,Chennai.<br/>
+<b>2021 </b>           : Completed Higher Secondary Education from Little Flower Convent Matriculation Higher Secondary School,Tiruppur.<br/>
+<b>2016 </b>           : Did My SSLC from Little Flower Convent Matriculation Higher Secondary School,Tiruppur.<br/>
+  
+
+<h3><b><u>SOFT SKILLS</u></b></h3>
+<b>STRENGTHNESS:</b><br/>
+
+<li>Communication<br/></li>
+<li>Decision Making<br/></li>
+<li>Team Spirit<br/></li>
+<li>Time Management<br/></li>
+<li>Punctualiy<br/></li>
+
+
+</div>
+  )
+}
+
+export default HomeComponent
